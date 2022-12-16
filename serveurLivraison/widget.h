adb97c5a078871042b0e6a158116713333ac54e5
dnalexen <dnalexen@yahoo.fr>
2022-12-16 10:49:04 +0100

    modif

--- serveurLivraison/widget.h
+++ serveurLivraison/widget.h
@@ -41,6 +41,6 @@ private:
     QTcpServer* mServer;
     QList<QTcpSocket*> mClients;
     QList<Camion*> mListCamions;
-    DbManager* mDB = new DbManager("Livraison.db");
+    DbManager* mDB = new DbManager("../serveurLivraison/Livraison.db");
 };
 #endif // WIDGET_H
